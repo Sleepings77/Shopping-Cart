74b83259fe6bd81ffdd7807061dd0182cc461dc5
Alejandro Castillo <alejandro.castillo@infinixsoft.com>
2021-07-15 17:16:46 -0300

    Initial Commit

---
+++ Shopping cart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping cart
+//
+//  Created by InfinixSoft on 15/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
